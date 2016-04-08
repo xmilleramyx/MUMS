948acbd2427840cc1df223110efddf1b4baed016
blairkoeneman <kkoeneman@scu.edu>
2016-04-08 13:46:30 -0700

    convert to hsv

--- js/testResults.js
+++ js/testResults.js
@@ -1,19 +1,127 @@
-var rVal = 0, gVal = 0, bVal = 0; 
- var computedH = 0;
- var computedS = 0;
- var computedV = 0;
+
+        var str = location.search;
+        var res = str.slice(5);
+        var myDataRef = new Firebase('https://mums.firebaseio.com/' + res);
+        
+        function profile_href() {
+            window.location.href = 'Profile.html?key=' + res;  
+        }
+        
+        function getTimestamp(){
+            var date = new Date();
+            currentDate = date.getDate();  
+            month = date.getMonth() + 1; 
+            year = date.getFullYear();
+            hour = date.getHours();
+            min = date.getMinutes();
+            sec = date.getSeconds();
+            return (month + "/" + currentDate + "/" + year + "  " + hour + ":" + min + ":" + sec);
+        }
+        
+        function makeTimeRef(){
+            var date = new Date();
+            currentDate = 100 - date.getDate();  
+            month = 100 - date.getMonth() + 1; 
+            year = 100 - date.getFullYear();
+            hour = 100 - date.getHours();
+            min = 100 - date.getMinutes();
+            sec = 100 - date.getSeconds();
+            return (month + "/" + currentDate + "/" + year + "  " + hour + ":" + min + ":" + sec);
+        }
+        
+        var timeStamp = getTimestamp();
+        var timeRef = makeTimeRef();
+        var glucose_hsv, ketone_hsv, nitrite_hsv, pH_hsv;
+        
+        
+        function add(){            
+            var testRef = myDataRef.child("tests");
+            testRef.push({timeRef: timeRef,
+                    timeStamp: timeStamp,
+                    pH: pH_hsv, 
+                    nitrite: nitrite_hsv,
+                    ketone: ketone_hsv,
+                    glucose: glucose_hsv
+            });  
+            alert("Saved results from: " + timeStamp);
+            myDataRef.child("lastTestRef").set({timeRef: timeRef,
+                    timeStamp: timeStamp,
+                    pH: pH_hsv, 
+                    nitrite: nitrite_hsv,
+                    ketone: ketone_hsv,
+                    glucose: glucose_hsv
+            });  
+        }
+        
+        
+        ///////////////////FAKE STRIP GENERATOR
+        function init() {        
+
+            var example = document.getElementById('example');
+            var context = example.getContext('2d');            
+
+            base_image = new Image();
+            base_image.src = 'img/test.png';
+            base_image.onload = function(){
+            context.drawImage(base_image, 0, 0);
+            }
+        }
+            
+        function calcResult(){
+            var example = document.getElementById('example');
+            var context = example.getContext('2d');
+    
+            //rgb functions
+            function avgRGB(p, x) {
+                sum = 0, count = 0;
+                for(var i=x; i<p.length; i+=4){
+                    sum += p[i];
+                    count ++;
+                }
+                return (Math.round(sum/count));
+            }
+ 
+            
+            function calc_hsv(x_pos, marker) {
+                var p = context.getImageData(x_pos, 2, 40, 39).data;
+                //var rgbText = "rgb(" + avgRGB(p, 0) + ", " + avgRGB(p, 1) + ", " + avgRGB(p, 2) + ")";
+                context.font = "12px Arial";
+                context.fillText(marker, x_pos, 55);
+                context.strokeStyle="gray";
+                context.strokeRect(x_pos, 2, 40, 39);
+                var hsvText = rgb2hsv(avgRGB(p, 0), avgRGB(p, 1), avgRGB(p, 2));
+                return hsvText;
+            }
+ 
+            //calc functions based on square position
+            glucose_hsv = calc_hsv(5, "Glucose"); //glucose = 1st square
+            ketone_hsv = calc_hsv(135, "Ketone"); //ketone = 3rd square
+            nitrite_hsv = calc_hsv(328, "Nitrite"); //nitrite = 6th square
+            pH_hsv = calc_hsv(524, "pH"); //pH = 9th square 
+ 
+            //display the values
+            timeStamp = getTimestamp();
+ 
+            $("#glucose_result").text("");
+            $("#glucose_result").append(glucose_hsv);
+            //$("#glucose_result").css("background-color", glucose_hsv);
+            $("#ketone_result").text("");
+            $("#ketone_result").append(ketone_hsv);
+            //$("#ketone_result").css("background-color", ketone_hsv);
+            $("#nitrite_result").text("");
+            $("#nitrite_result").append(nitrite_hsv);
+            //$("#nitrite_result").css("background-color", nitrite_hsv);
+            $("#pH_result").text("");
+            $("#pH_result").append(pH_hsv);
+            //$("#pH_result").css("background-color", pH_hsv);
+        }
+
+        //load all HTML before javascript
+        document.addEventListener("DOMContentLoaded", init, false);
 
         
-function input(){
-    rVal = prompt("Enter r");
-    gVal = prompt("Enter g");
-    bVal = prompt("Enter b");
-    var answer = rgb2hsv(rVal, gVal, bVal);
-    alert(answer);
-}
-        
+
 function rgb2hsv (r,g,b) {
-
  //remove spaces from input RGB values, convert to int
  var r = parseInt( (''+r).replace(/\s/g,''),10 ); 
  var g = parseInt( (''+g).replace(/\s/g,''),10 ); 
@@ -44,6 +152,6 @@ function rgb2hsv (r,g,b) {
  computedH = 60*(h - d/(maxRGB - minRGB));
  computedS = (maxRGB - minRGB)/maxRGB;
  computedV = maxRGB;
- return("HSV( " + computedH + " , "  + computedS +  " , "  + computedV);
+ return("HSV( " + computedH + " , "  + computedS +  " , "  + computedV + " )");
 }
 
